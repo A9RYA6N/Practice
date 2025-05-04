d82deba6358b39f2c18adfbb11ebfdef53b55a17
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-04 17:43:03 +0530

    Fix thanks page

--- frontend/src/components/Thanks.jsx
+++ frontend/src/components/Thanks.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router'
 const Thanks = () => {
+  const navigate = useNavigate();
   return (
     <div>
       You have logged in succesfully
+      <button onClick={()=>navigate('/')}>Go back to homepage</button>
     </div>
   )
 }
