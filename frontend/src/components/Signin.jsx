66e4051fd7f29d6495b9e43b3ebe5dd118e90969
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-04 20:30:21 +0530

    Added disabled buttons part 3

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -36,7 +36,7 @@ const Signin = () => {
             <label htmlFor='password' class="block mb-2 text-sm font-medium text-gray-900">Password</label>
             <input id='password' type="password" placeholder='Enter your password' class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>setPass(e.target.value)}/>
 
-            <button onClick={handleClick} class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" disabled={loading}>{loading ? "Submitting" : "Submit"}</button>
+            <button onClick={handleClick} disabled={loading} class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" >{loading ? "Submitting" : "Submit"}</button>
         </div>
     )
 }
