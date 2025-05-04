d6bfc911501c7da5c9dc64f18ff6a3218b229666
A9RYA6N <aryanburnwal8@gmail.com>
2025-05-04 20:26:14 +0530

    Added disabled buttons part 2

--- frontend/src/components/CreateProduct.jsx
+++ frontend/src/components/CreateProduct.jsx
@@ -53,7 +53,7 @@ const CreateProduct = () => {
             <label htmlFor="link" class="block mb-2 text-sm font-medium text-gray-900">Image Link</label>
             <input id='link' type="text" placeholder='Enter image link' class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>{setImg(e.target.value)}}/>
 
-            <button onClick={handleClick} class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" disabled={loading}>Submit</button>
+            <button onClick={handleClick} class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" disabled={loading}>{loading ? "Submitting" : "Submit"}</button>
         </div>
     )
 }
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -39,7 +39,7 @@ const Signup = () => {
       <label htmlFor='number' class="block mb-2 text-sm font-medium text-gray-900">Phone</label>
       <input id='number' type="number" placeholder='Enter your number' class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>{setPhone(e.target.value)}}/>
 
-      <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handleClick} disabled={loading}>Submit</button>
+      <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handleClick} disabled={loading}>{loading ? "Submitting" : "Submit"}</button>
     </div>
   )
 }
